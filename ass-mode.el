d73b9eb5c2bebcb98bb9d668fdc4b6d451c3f3c3
disbeliever <nerevar101@mail.ru>
2011-11-09 15:54:03 +0400

    Started implementing of getting styles list

--- ass-mode.el
+++ ass-mode.el
@@ -4,8 +4,6 @@
   )
 
 
-;; (modify-syntax-entry ?; "<")
-
 (defvar ass-font-lock-keywords
   (list
    '("\\;.*" . 'font-lock-comment-face)
@@ -18,6 +16,10 @@
   (concat (file-name-sans-extension (buffer-file-name))
           ext
           ))
+
+(defun ass-get-subtitles-from-mkv (file-name)
+  (process-lines "mkvmerge" "--identify" file-name)
+  )
 
 (defun ass-get-video-name ()
   "Construct the name of the video file"
@@ -33,6 +35,29 @@
   (nth 1 (split-string (thing-at-point 'line) ","))
   )
 
+
+(defun ass-style-get-format ()
+  ""
+  (interactive)
+  (print (nth 1 (split-string (ass-get-styles-list) "\n")))
+  )
+(defun ass-get-styles-list ()
+  ""
+  (defvar point-start (search-forward-regexp "\\[V4\\+?.+\\]" nil t))
+  (goto-char 0)
+  (defvar point-end (search-forward-regexp "\\[V4\\+?\\(.*\n\\)*\\[" nil t))
+  (buffer-substring-no-properties point-start point-end
+                                  ))
+
+(defun ass-create-list-of-styles-buffer ()
+  "Creates buffer with list of ASS styles"
+  ; (generate-new-buffer "ASS Styles")
+  (with-output-to-temp-buffer "ASS Styles"
+    (with-current-buffer "ASS Styles"
+      (print "Style name")
+      (ass-mode)))
+  )
+
 (defun mplayer ()
   "Run mplayer"
   (interactive)
@@ -42,6 +67,7 @@
 
 (defvar ass-mode-map (make-keymap))
 (define-key ass-mode-map "\C-c\C-o" 'mplayer)
+(define-key ass-mode-map "\C-c\C-l" 'ass-style-get-format)
 (use-local-map ass-mode-map)
 
 (provide 'ass-mode)
